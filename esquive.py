0799be06cf7a56d69c46bd1dc2a8a94a516ff922
PGII33 <mickycomptepro@gmail.com>
2023-10-04 21:42:40 +0200

    Esquive :  - Personnage qui se déplace  - N'apparait plus  - Tailles en fonction de la taille de l'écran de l'utilisateur

--- esquive.py
+++ esquive.py
@@ -2,15 +2,61 @@
 import pygame as pygame
 import display_class as display
 
-def esquive_game(screen:pygame.Surface, running, coef)-> None:
-    test_button = display.Button((0,0), (20,40),(122,122,122))
-    pos = 20*coef[0], 20*coef[1]
+class Player:
+    """ The player """
+    def __init__(self, pos:list, dim:list):
+        self.pos = pos
+        self.dim = [0,0,dim[0],dim[1]]
+        self.rect = pygame.Rect(pos[0], pos[1], self.dim[2]-self.dim[0], self.dim[3]-self.dim[1])
+    
+    def draw(self,screen:pygame.Surface):
+        """ Draw the player """
+        pygame.draw.rect(screen, (0,0,0), self.rect)
+
+def esquive_game(screen:pygame.Surface, running:bool, coef:tuple)-> None:
+
+    # Initialisation des variables
+    player = Player([1920*coef[0]//2, 1080*coef[1]//2], [20*coef[0], 20*coef[1]])
+    mov_up = False
+    mov_down = False
+    mov_left = False
+    mov_right = False
+
+    # Boucle while
     while running :
-        player = pygame.Rect(pos[0], pos[1], 50*coef[0],50*coef[1])
+        screen.fill((120,50,70))
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_e:
                     running = False
-        test_button.show(screen)
-        screen.fill((120,50,70))
+                if event.key == pygame.K_UP:
+                    mov_up = True
+                if event.key == pygame.K_DOWN:
+                    mov_down = True
+                if event.key == pygame.K_LEFT:
+                    mov_left = True
+                if event.key == pygame.K_RIGHT:
+                    mov_right = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    mov_up = False
+                if event.key == pygame.K_DOWN:
+                    mov_down = False
+                if event.key == pygame.K_LEFT:
+                    mov_left = False
+                if event.key == pygame.K_RIGHT:
+                    mov_right = False
+        
+        # Gestion des mouvements
+        if mov_up:
+            player.pos[1] -= 1*coef[1]
+        if mov_down:
+            player.pos[1] += 1*coef[1]
+        if mov_left:
+            player.pos[0] -= 1*coef[0]
+        if mov_right:
+            player.pos[0] += 1*coef[0]
+
+        print(player.pos, player.dim)
         pygame.display.update()
--- main.pyw
+++ main.pyw
@@ -7,8 +7,8 @@ import esquive
 pygame.init()
 screen = pygame.display.set_mode()
 SCREEN_WEIGHT, SCREEN_HEIGHT = screen.get_size()
-WEIGHT_COEF = 1920/SCREEN_WEIGHT
-HEIGHT_COEF = 1080/SCREEN_HEIGHT
+WEIGHT_COEF = SCREEN_WEIGHT/1920
+HEIGHT_COEF = SCREEN_HEIGHT/1080
 COEF = (WEIGHT_COEF, HEIGHT_COEF)
 pygame.quit()
 
@@ -18,6 +18,7 @@ screen = pygame.display.set_mode((SCREEN_WEIGHT//2, SCREEN_HEIGHT//2))
 running = True
 
 while running:
+    screen.fill((123,123,123))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
@@ -25,7 +26,6 @@ while running:
             elif event.key == pygame.K_a:
                 esquive.running = True
                 esquive.esquive_game(screen, True, COEF)
-    screen.fill((123,123,123))
     pygame.display.update()
 
 pygame.display.quit()
